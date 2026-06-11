2b7369c2b96f88af1186b2f6cbbe9a0cae5c6faa
ShuotingZhang <295914045@qq.com>
2026-06-11 17:50:12 +0800

    lead the selected buttons more obvious

--- filters.js
+++ filters.js
@@ -1,11 +1,7 @@
 const filterButtons = document.querySelectorAll(".filter-options button");
 
 filterButtons.forEach(function(button) {
-
     button.addEventListener("click", function() {
-
         button.classList.toggle("selected");
-
     });
-
 });
