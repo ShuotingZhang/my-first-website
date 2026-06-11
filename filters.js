00f0c9704bda3d480fdb07d9991035817511bfa6
ShuotingZhang <295914045@qq.com>
2026-06-11 17:39:19 +0800

    make the filter sections can be selectesd.

---
+++ filters.js
@@ -0,0 +1,11 @@
+const filterButtons = document.querySelectorAll(".filter-options button");
+
+filterButtons.forEach(function(button) {
+
+    button.addEventListener("click", function() {
+
+        button.classList.toggle("selected");
+
+    });
+
+});
